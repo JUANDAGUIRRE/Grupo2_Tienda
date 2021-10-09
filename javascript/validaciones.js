3c4b9105a637a0d15de573e5d9dca62417c9001b
Jose Salazar <68127908+Jose-SC@users.noreply.github.com>
2021-10-08 21:32:31 -0500

    creacion de funciones
    se crean funciones para sprint 2
    pendiente de evaluar su validez

--- javascript/validaciones.js
+++ javascript/validaciones.js
@@ -1,31 +1,51 @@
 function validar_nombre_usuario(string){
     const string = document.getElementById("nombre_usuario");
-    const alphaExp = /^[a-zA-Z]+$/;
-
+    const alphaExp = /^[a-zA-Z]+$/i;
     
-            if(string.match(alphaExp))
-            {
-                if(string.charAt(0)===string.charAt(0).toUpperCase());
-                return true;
-            }
-            else{
-                return false;
-            }
+    if(string.match(alphaExp))
+    {
+        if(string.charAt(0)===string.charAt(0).toUpperCase());
+        alert("falso");
+    }
+    else{
+        alert("verdadero");
+    }
 
 }
 
-validar_nombre_usuario(jose);
-
-
 function validar_anoNacimiento_usuario(valor){
-    const value = document.getElementById("anoNacimiento_usuario")
+    const valor = document.getElementById("anoNacimiento_usuario").value;
 
+    if(isNaN(valor)){
+        return falso;
+    }
+    else{
+        if(valor >= 1900 && valor <= 2021){
+            return verdadero;
+        }
+        else{
+            return falso;
+        }
+    }
 }
 
 function validar_contrasena(string){
-    const string = document.getElementById("contrasena_usuario")
+    const string = document.getElementById("contrasena_usuario").value;
+    const alphaNum = /^[a-z0-9]+$/i;
+    const alphaNumEsp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+
+    if(string.lenght>=6 && string.match(alphaNum) ){
+        if(string.match(alphaNumEsp)){
+            return verdadero;
+        }
+        else{
+            return falso;
+        }
+    }
+    return falso;
+
 }
 
 module.exports.validar_nombre_usuario = validar_nombre_usuario;
-module.exports.validar_contrasena=validar_contrasena;
+module.exports.validar_contrasena = validar_contrasena;
 module.exports.validar_anoNacimiento_usuario = validar_anoNacimiento_usuario;
