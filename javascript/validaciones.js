48b83e8f2b723aa83a5ce38bb96704171c936b3f
Jose Salazar <68127908+Jose-SC@users.noreply.github.com>
2021-10-09 14:38:46 -0500

    Actualización código
    Con este código se pasan 3 de los 4 requerimientos del bot calificador

--- javascript/validaciones.js
+++ javascript/validaciones.js
@@ -1,51 +1,50 @@
-function validar_nombre_usuario(string){
-    const string = document.getElementById("nombre_usuario");
-    const alphaExp = /^[a-zA-Z]+$/i;
+function validar_nombre_usuario(nombre){
+    var nombre = document.getElementById("nombre_usuario").value;
+    var alphaExp = /^[a-zA-Z]+$/i;
     
-    if(string.match(alphaExp))
+    if(nombre.match(alphaExp))
     {
-        if(string.charAt(0)===string.charAt(0).toUpperCase());
-        console.log("verdadero");
+        if(nombre.charAt(0)===nombre.charAt(0).toUpperCase());
+        return true;
     }
     else{
-        console.log("falso");
+        return false;
     }
 
 }
 
 function validar_anoNacimiento_usuario(valor){
-    const valor = document.getElementById("anoNacimiento_usuario").value;
+    var valor = document.getElementById("anoNacimiento_usuario").value;
 
     if(isNaN(valor)){
-        console.log("falso");
+        return false;
     }
     else{
         if(valor >= 1900 && valor <= 2021){
-            console.log("verdadero");
+            return true;
         }
         else{
-            console.log("falso");
+            return false;
         }
     }
 }
 
-function validar_contrasena(string){
-    const string = document.getElementById("contrasena_usuario").value;
-    const alphaNum = /^[a-z0-9]+$/i;
-    const alphaNumEsp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+function validar_contrasena(contrasena){
+    var contrasena = document.getElementById("contrasena_usuario").value;
+    //const alphaNum = /^[a-z0-9]+$/i;
+    const alphaNumEsp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/i;
 
-    if(string.lenght>=6 && string.match(alphaNum) ){
-        if(string.match(alphaNumEsp)){
-            console.log("verdadero");
-        }
-        else{
-            console.log("falso");
-        }
+    if(contrasena.lenght >= 6 && contrasena.match(alphaNumEsp)){
+        return true;
+
     }
-    console.log("falso");
+    else{
+        return false
+    }
+    
 
 }
 
-module.exports.validar_nombre_usuario = validar_nombre_usuario;
-module.exports.validar_contrasena = validar_contrasena;
-module.exports.validar_anoNacimiento_usuario = validar_anoNacimiento_usuario;
+module.exports.validar_nombre_usuario = validar_nombre_usuario
+module.exports.validar_anoNacimiento_usuario = validar_anoNacimiento_usuario
+module.exports.validar_contrasena = validar_contrasena
